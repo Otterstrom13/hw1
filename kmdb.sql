95b206f597bff34fdaac95035142d334e5aa29c3
Ethan Otterstrom <ethan.otterstrom@kellogg.northwestern.edu>
2024-01-21 03:42:12 +0000

    Draft 1 - Domain Model

--- kmdb.sql
+++ kmdb.sql
@@ -106,13 +106,66 @@
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
 
+
+
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS characters;
+
 -- Create new tables, according to your domain model
 -- TODO!
+
+CREATE TABLE studios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year INTEGER,
+  rating TEXT,
+  studio_id INTEGER
+);
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  character TEXT,
+  actor_id INTEGER,
+  movie_id INTEGER
+);
+
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
 
+INSERT INTO studios (
+    name
+)
+VALUES (
+    "Warner Bros."
+);
+
+INSERT INTO movies (
+    title,
+    year,
+    rating,
+    studio_id
+)
+VALUES (
+    "Batman Begins",
+    "2005",
+    "PG-13",
+    "1"
+)
+;
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
